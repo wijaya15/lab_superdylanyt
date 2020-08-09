dc85c11794fa8448f3efcbe2667c5f6118e4bb4f
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-08-08 21:23:33 -0500

    Update lab_superdylanyt.sh

--- lab_superdylanyt.sh
+++ lab_superdylanyt.sh
@@ -116,8 +116,9 @@ cat > $update <<- EOM
 clear
 rm lab_superdylanyt.sh
 wget https://raw.githubusercontent.com/dylan14567/lab_superdylanyt/master/lab_superdylanyt.sh
-chmod +x lab_superdylanyt
+chmod +x lab_superdylanyt.sh
 clear
+rm -rf update-lab_superdylanyt.sh
 EOM
 
 chmod +x $update
