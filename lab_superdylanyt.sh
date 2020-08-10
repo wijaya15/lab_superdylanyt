dd2ac65153cf35979e1de56f14d965105a38a9bb
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-08-10 17:56:35 -0500

    Update lab_superdylanyt.sh

--- lab_superdylanyt.sh
+++ lab_superdylanyt.sh
@@ -203,8 +203,8 @@ fi
 "7")
 clear
 pkg update && pkg upgrade
-mkdir Ubuntu
-cd Ubuntu
+mkdir $HOME/Ubuntu
+cd $HOME/Ubuntu
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 clear
 bin=startubuntu
