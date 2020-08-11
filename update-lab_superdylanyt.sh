ab60df7b0cd4e1d6a436a4359eaaacbfc855ced2
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-08-11 13:08:44 -0500

    Create update-lab_superdylanyt.sh

---
+++ update-lab_superdylanyt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+rm lab_superdylanyt.sh
+wget https://raw.githubusercontent.com/dylan14567/lab_superdylanyt/master/lab_superdylanyt.sh
+chmod +x lab_superdylanyt.sh
+clear
+rm -rf update-lab_superdylanyt.sh
+             
