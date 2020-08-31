27518a181ee770c59edf316cb3cd20a6bf9ed75b
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-08-31 10:21:55 -0500

    Create SHELLSCRIPT.c

---
+++ SHELLSCRIPT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SHELLSCRIPT "\
+#/bin/bash \n\
+echo -e \"\" \n\
+echo -e \"Hemos añadido bash en codigo C!!\" \n\
+read -p \"Pulsa <enter> para continuar\" \n\
+clear\
+"
+
+int main() {
+
+system(SHELLSCRIPT);
+return 0;
+}
