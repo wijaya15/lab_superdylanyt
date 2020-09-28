ac771a5df6200ba054b7de37435e9e31b9461af0
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-09-27 19:44:02 -0500

    Update update-lab_superdylanyt.sh

--- update-lab_superdylanyt.sh
+++ update-lab_superdylanyt.sh
@@ -5,5 +5,6 @@ echo " [+] Actualizando lab_superdylanyt "
 curl --progress-bar -L --fail --retry 4 -O https://raw.githubusercontent.com/dylan14567/lab_superdylanyt/master/lab_superdylanyt.sh
 chmod +x lab_superdylanyt.sh
 clear
+echo " [+] Actualización Completada "
 rm -rf update-lab_superdylanyt.sh
              
