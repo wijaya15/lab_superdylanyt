54db207a69317d33102caea247a0edf7b9262b71
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-10-22 11:06:34 -0500

    Update java.sh

--- Server/java.sh
+++ Server/java.sh
@@ -6,20 +6,20 @@ jre=openjdk-9-jre.deb
 echo " [+] Downloading the .deb files "
   case `dpkg --print-architecture` in
 	aarch64)
-		archname="aarch64"; jre="aarch64" ;;
+		archname="aarch64"; jre1="aarch64" ;;
 	arm64)
-		archname="aarch64"; jre="aarch64" ;;
+		archname="aarch64"; jre1="aarch64" ;;
 	armhf)
-		archname="arm"; jre="arm" ;;
+		archname="arm"; jre1="arm" ;;
 	armv7l)
-		archname="arm"; jre="arm" ;;
+		archname="arm"; jre1="arm" ;;
 	arm)
-		archname="arm"; jre="arm" ;;
+		archname="arm"; jre1="arm" ;;
 	*)
 		echo " [+] unknown architecture"; exit 1 ;;
    esac
    wget -c --quiet --show-progress "https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jdk-headless_9.2017.8.20-1_$archname.deb" -O $jdk
-   wget -c --quiet --show-progress "https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jre-headless_9.2017.8.20-1_$jre.deb" -O $jre
+   wget -c --quiet --show-progress "https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jre-headless_9.2017.8.20-1_$jre1.deb" -O $jre
    
    echo " [+] Installing Java... "
    apt update -y && apt upgrade -y
