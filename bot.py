5a66112458be91bb0512cdc6c97914d4b18862b3
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-11-29 11:24:47 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import time
+
+while True:
+    dato = input ("tu: ")
+
+    if dato == 'hola':
+        print ("Hola")
+    elif dato == 'como estas':
+        print ("Estoy Bien y Tu ??")
+    elif dato == 'igual estoy bien':
+        print ("ok")
+    elif dato == 'que puedes hacer':
+        print ('puedo hacer muchas cosas')
+    elif dato == 'bye' or dato == 'adios':
+        print ("bye :c")
+        break
+    elif dato == 'quien te creo':
+        print ("Me creo DYLAN1456732")
+    else:
+        print ("Error")
+
